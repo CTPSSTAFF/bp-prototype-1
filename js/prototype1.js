0ce78fa64322e1dd17464927a7fb6f342584b21f
Ben Krepp <bkrepp@ctps.org>
2023-06-01 14:47:41 -0400

    Boston is the hub of the prototype.

--- js/prototype1.js
+++ js/prototype1.js
@@ -1,5 +1,10 @@
-// 'Getting started' code taken from leaflet.js website
-var map = L.map('map').setView([51.505, -0.09], 13);
+// 'Getting started' code taken from leaflet.js website, and modified
+
+var regionCenterLat = 42.345111165; 
+var regionCenterLng = -71.124736685; 
+var initialZoom = 11; 
+
+var map = L.map('map').setView([regionCenterLat, regionCenterLng], initialZoom);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
