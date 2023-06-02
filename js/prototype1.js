9a3c99bd03dc55c9d983b7549565bbe18b1c7ac3
Ben Krepp <bkrepp@ctps.org>
2023-06-02 10:19:07 -0400

    Default leaflet markers placed correctly

--- js/prototype1.js
+++ js/prototype1.js
@@ -7,9 +7,12 @@ var initialZoom = 11;
 // Leaflet 'map' Object
 var map = {};
 
+// Complete GeoJSON 'blob' for count locations
+var countlocs_geojson = {};
+
 // Arrays of GeoJSON features for count locations and counts
-var countlocs = [],
-    counts = [];
+var countlocs_features = [],
+    counts_features = [];
 	
 // Danfo dataframes for count locations and counts
 var countlocs_df = {},
@@ -17,11 +20,47 @@ var countlocs_df = {},
 
 function main_app() {
 	map = L.map('map').setView([regionCenterLat, regionCenterLng], initialZoom);
-	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	const osm_base_layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
+	
 	var _DEBUG_HOOK_ = 0;
+	
+	////////////////////////////////////
+	// Add count locations to map
+	
+	/*
+	var geojsonMarkerOptions = {
+		radius: 8,
+		fillColor: "#ff7800",
+		color: "#000",
+		weight: 1,
+		opacity: 1,
+		fillOpacity: 0.8
+	};
+	
+	L.geoJSON(countlocs, {
+		pointToLayer: function (feature, latlng) {
+			return L.circleMarker(latlng, geojsonMarkerOptions);
+		}
+	}).addTo(map);
+	*/
+	
+	function onEachFeature(feature, layer) {
+		let popupContent = `<p>I started out as a GeoJSON ${feature.geometry.type}, but now I'm a Leaflet vector!</p>`;
+
+		if (feature.properties && feature.properties.popupContent) {
+			popupContent += feature.properties.popupContent;
+		}
+
+		layer.bindPopup(popupContent);
+	}
+	
+	const countlocs_layer = L.geoJSON(countlocs_geojson, {
+	}).addTo(map);
+	
+	_DEBUG_HOOK_ = 1;
 } // main_app()
 
 var pointsURL = 'data/json/ctps_bp_count_locations_pt.geo.json',
@@ -37,20 +76,21 @@ function initialize() {
             alert("One or more requests to load data failed. Exiting application.");
             return; 
         }
-		countlocs = bp_countlocs[0].features;
+		// The following line is probably not needed
+		countlocs_geojson = bp_countlocs; 
+		
+		countlocs_features = bp_countlocs[0].features;
+		
 		// Note: the count data for each count 'feature' is found in counts[i].properties;
 		//       it needs to be 'hoisted up' one level (so to speak) before being loaded
 		//       into the 'counts' array
-		var count_features = bp_counts[0].features;
-		count_features.forEach(function(feature) {
-			counts.push(feature.properties);
+		counts_features = bp_counts[0].features;
+		counts_features.forEach(function(feature) {
+			counts_features.push(feature.properties);
 		});
 		// Convert JSON arrays to Danfo data frames
-		countlocs_df = new dfd.DataFrame(countlocs);
-		counts_df = new dfd.DataFrame(counts);
-		
-		// temp
-		countlocs_df.print()
+		countlocs_df = new dfd.DataFrame(countlocs_features);
+		counts_df = new dfd.DataFrame(counts_features);
 		
 		var _DEBUG_HOOK_ = 0;
 		main_app();
